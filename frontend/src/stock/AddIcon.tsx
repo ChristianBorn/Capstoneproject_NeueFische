64dec7a0259e97e0914be8db8bd6665907b82502
christianborn <chrisborn@hotmail.de>
2022-11-21 20:31:56 +0100

    Frontend: create component for green add-button

---
+++ frontend/src/stock/AddIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCirclePlus} from "@fortawesome/free-solid-svg-icons";
+
+function AddIcon() {
+    return (
+        <a  href={"/lager/ueberblick"}>
+                        <span className={"add-record-icon"}>
+                    <FontAwesomeIcon icon={faCirclePlus} />
+                            </span>
+        </a>
+    );
+}
+
+export default AddIcon;
