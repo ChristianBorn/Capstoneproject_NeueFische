7828715d0a7eed53ff7cc9694a031ade51e87f6c
christianborn <chrisborn@hotmail.de>
2022-11-21 15:20:52 +0100

    Fix tests

--- backend/src/main/java/de/ffmjava/capstone/backend/BackendApplication.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/BackendApplication.java
@@ -4,8 +4,6 @@ package de.ffmjava.capstone.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class BackendApplication {
 
