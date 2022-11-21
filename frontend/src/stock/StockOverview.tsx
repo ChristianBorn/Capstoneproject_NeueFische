4341407f4c585ab36ea3f443340e4d9112a94363
christianborn <chrisborn@hotmail.de>
2022-11-21 17:17:22 +0100

    Frontend: Create Stock Overview

--- frontend/src/stock/StockOverview.tsx
+++ frontend/src/stock/StockOverview.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function StockOverview() {
+
+    const [stockItems, setStockItems] = useState<[]>([])
+
+    const getAllStockItems = () => {
+        axios.get("/lager/ueberblick")
+            .then((response) => response.data)
+            .catch((error) => console.error("Error while getting Stockitems:" + error))
+            .then(data => setStockItems(data))
+    }
+
+    useEffect(() => {
+        getAllStockItems()
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            {stockItems.length > 0 ? stockItems.map(singleItem => <div>{singleItem}</div>)
+            :
+            <p>Keine Items im Lager</p>}
+        </div>
     );
 }
 
