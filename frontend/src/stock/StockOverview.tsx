519489668640e3b2528d7cd48853286a0f4b6e21
christianborn <chrisborn@hotmail.de>
2022-11-21 20:31:09 +0100

    Frontend: Add Table for displaying Stockitems

--- frontend/src/stock/StockOverview.tsx
+++ frontend/src/stock/StockOverview.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import "./css/StockOverview.css";
+import {StockItemModel} from "./StockItemModel";
+import AddIcon from "./AddIcon";
 
 function StockOverview() {
 
-    const [stockItems, setStockItems] = useState<[]>([])
+    const [stockItems, setStockItems] = useState<StockItemModel[]>([])
 
     const getAllStockItems = () => {
         axios.get("/lager/ueberblick")
@@ -15,27 +15,54 @@ function StockOverview() {
             .then(data => setStockItems(data))
     }
 
+
     useEffect(() => {
         getAllStockItems()
     }, [])
 
+
+
     return (
-        <div>
-            {stockItems.length > 0 ? stockItems.map(singleItem => <div>{singleItem}</div>)
+        <>
+            {stockItems.length > 0 ?
+                <>
+                <div className={"stock-overview-table-wrapper"}>
+                    <table>
+                    <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Typ</th>
+                    <th>Vergübare Menge in Kilogramm</th>
+                    <th>Preis pro Kilogramm</th>
+                    <th>Täglicher Verbrauch</th>
+                    <th>Aufgebraucht in</th>
+                    <th>Aktionen</th>
+                </tr>
+                    {stockItems.map(singleItem => {
+                        return <tr key={singleItem.id}>
+                            <td>{singleItem.name}</td>
+                            <td>{singleItem.type}</td>
+                            <td>{singleItem.amountInStock}</td>
+                            <td>{singleItem.pricePerKilo}</td>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                        </tr>
+                    })
+                }
+                    </tbody>
+                </table>
+
+                </div>
+                <AddIcon/></>
             :
             <div>
                 <p>Keine Items im Lager</p>
-
-                <a  href={"/lager/uebersicht"}>
-                        <span className={"add-record-icon"}>
-                    <FontAwesomeIcon icon={faCirclePlus} />
-                            </span>
-                    </a>
-
+                <AddIcon/>
             </div>
 
             }
-        </div>
+</>
     );
 }
 
