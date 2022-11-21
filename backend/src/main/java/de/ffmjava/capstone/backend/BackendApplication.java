6e26a93c91f7ae1ea4eb2175d0dc158c62eb8f8b
christianborn <chrisborn@hotmail.de>
2022-11-21 12:30:04 +0100

    Test for sonarcloud analysis

--- backend/src/main/java/de/ffmjava/capstone/backend/BackendApplication.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/BackendApplication.java
@@ -1,7 +1,10 @@
 package de.ffmjava.capstone.backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BackendApplication {
