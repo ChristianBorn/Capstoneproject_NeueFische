36bf4eb6d88de348ae2f044e90940dea692476b4
christianborn <chrisborn@hotmail.de>
2022-11-22 15:20:27 +0100

    Backend: Modify ENUM StockType.java to match regular display strings (not all caps)

--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockType.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockType.java
@@ -1,6 +1,18 @@
 package de.ffmjava.capstone.backend.stock;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StockType {
-    FUTTER,
-    EINSTREU,
+    FUTTER("Futter"),
+    EINSTREU("Einstreu"),
+    ;
+    private final String displayName;
+
+    StockType(String displayName)    {
+        this.displayName = displayName;
+    }
+    @JsonValue
+    public String getDisplayName() {
+        return displayName;
+    }
 }
