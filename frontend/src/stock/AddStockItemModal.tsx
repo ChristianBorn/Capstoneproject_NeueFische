5633609682cdab2624301bdce68d7286d685073d
christianborn <chrisborn@hotmail.de>
2022-11-22 20:30:01 +0100

    Frontend: Add change handling to form

--- frontend/src/stock/AddStockItemModal.tsx
+++ frontend/src/stock/AddStockItemModal.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import Modal from 'react-modal';
+import {StockItemModel} from "./StockItemModel";
 
 type ModalProps = {
     modalIsOpen: boolean,
@@ -7,6 +8,32 @@ type ModalProps = {
 }
 
 function AddStockItemModal(props: ModalProps) {
+    const [newStockItem, setNewStockItem] = useState<StockItemModel>({
+        id: "", name: "", amountInStock: 0, pricePerKilo: 0, type: ""
+    })
+
+    const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log(newStockItem)
+    }
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setNewStockItem({
+            ...newStockItem,
+            [name]: value
+        })
+    }
+
+    const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setNewStockItem({
+            ...newStockItem,
+            [name]: value
+        })
+    }
+
     return (
         <Modal
             isOpen={props.modalIsOpen}
@@ -15,29 +42,31 @@ function AddStockItemModal(props: ModalProps) {
             // style={customStyles}
             contentLabel="Example Modal"
         >
-        <section >
-            <button className={"modal-close-button"} onClick={() => props.closeModal()}>Schließen</button>
-            <form onSubmit={event => console.log(event)}>
+            <section>
+                <button className={"modal-close-button"} onClick={() => props.closeModal()}>Schließen</button>
+                <form onSubmit={handleSubmit}>
 
-                <label htmlFor={"name"}>Name/Bezeichnung</label>
-                <input required type={"text"} id={"name"} name={"name"}/>
+                    <label htmlFor={"name"}>Name/Bezeichnung</label>
+                    <input onChange={handleChange} required type={"text"} id={"name"} name={"name"}/>
 
-                <label htmlFor={"price"}>Preis pro Kilogramm</label>
-                <input placeholder={"0"} step={"0.1"} min={"0"} required type={"number"} id={"price"} name={"price"}/>
+                    <label htmlFor={"price"}>Preis pro Kilogramm</label>
+                    <input onChange={handleChange} placeholder={"0"} step={"0.1"} min={"0"} required type={"number"}
+                           id={"price"} name={"pricePerKilo"}/>
 
-                <label htmlFor={"amount"}>Menge in Kilogramm</label>
-                <input placeholder={"0"} step={"0.1"} min={"0"} required type={"number"} id={"amount"} name={"amount"}/>
+                    <label htmlFor={"amount"}>Menge in Kilogramm</label>
+                    <input onChange={handleChange} placeholder={"0"} step={"0.1"} min={"0"} required type={"number"}
+                           id={"amount"} name={"amountInStock"}/>
 
-                <label htmlFor={"type"}>Typ:</label>
-                <select required id={"type"} name={"type"}>
-                    <option disabled value={""}>Bitte auswählen</option>
-                    <option value={"Futter"}>Futter</option>
-                    <option value={"Einstreu"}>Einstreu</option>
-                </select>
-                <input type={"submit"} value={"Einlagern"}/>
-            </form>
-        </section>
-            </Modal>
+                    <label htmlFor={"type"}>Typ:</label>
+                    <select onChange={handleSelectChange} required id={"type"} name={"type"}>
+                        <option selected disabled value={""}>Bitte auswählen</option>
+                        <option value={"Futter"}>Futter</option>
+                        <option value={"Einstreu"}>Einstreu</option>
+                    </select>
+                    <input type={"submit"} value={"Einlagern"}/>
+                </form>
+            </section>
+        </Modal>
     );
 }
 
