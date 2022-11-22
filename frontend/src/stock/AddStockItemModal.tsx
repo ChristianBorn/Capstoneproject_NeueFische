efc2b3032d2ef5ac154f50a95a9e309271a37761
christianborn <chrisborn@hotmail.de>
2022-11-22 18:59:20 +0100

    Frontend: Add form to modal

--- frontend/src/stock/AddStockItemModal.tsx
+++ frontend/src/stock/AddStockItemModal.tsx
@@ -16,9 +16,25 @@ function AddStockItemModal(props: ModalProps) {
             contentLabel="Example Modal"
         >
         <section >
-            <button onClick={() => props.closeModal()}>Schließen</button>
-            <form>
-                <input type={"text"}/>
+            <button className={"modal-close-button"} onClick={() => props.closeModal()}>Schließen</button>
+            <form onSubmit={event => console.log(event)}>
+
+                <label htmlFor={"name"}>Name/Bezeichnung</label>
+                <input required type={"text"} id={"name"} name={"name"}/>
+
+                <label htmlFor={"price"}>Preis pro Kilogramm</label>
+                <input placeholder={"0"} step={"0.1"} min={"0"} required type={"number"} id={"price"} name={"price"}/>
+
+                <label htmlFor={"amount"}>Menge in Kilogramm</label>
+                <input placeholder={"0"} step={"0.1"} min={"0"} required type={"number"} id={"amount"} name={"amount"}/>
+
+                <label htmlFor={"type"}>Typ:</label>
+                <select required id={"type"} name={"type"}>
+                    <option disabled value={""}>Bitte auswählen</option>
+                    <option value={"Futter"}>Futter</option>
+                    <option value={"Einstreu"}>Einstreu</option>
+                </select>
+                <input type={"submit"} value={"Einlagern"}/>
             </form>
         </section>
             </Modal>
