85d6b3bfcca75a5ee855df4696e7660f1124015e
christianborn <chrisborn@hotmail.de>
2022-11-22 16:39:57 +0100

    Backend: Add tests for addNewStockItem

--- backend/src/test/java/de/ffmjava/capstone/backend/stock/StockIntegrationTest.java
+++ backend/src/test/java/de/ffmjava/capstone/backend/stock/StockIntegrationTest.java
@@ -1,13 +1,17 @@
 package de.ffmjava.capstone.backend.stock;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,6 +23,8 @@ class StockIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void getAllStockItems_AndExpectEmptyList_200() throws Exception {
@@ -26,5 +32,40 @@ class StockIntegrationTest {
                         ("/lager/ueberblick"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void addNewStockItem_AndExpectStockItem_201() throws Exception {
+        String jsonString =
+                """
+                    {
+                      "name": "Pellets",
+                      "type": "Futter",
+                      "amountInStock": 42.0,
+                      "pricePerKilo": 42.0
+                    }
+                """;
+        String apiResponse = mockMvc.perform(post("/lager/ueberblick")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonString)
+                )
+                .andExpect(status().is(201))
+                .andReturn().getResponse().getContentAsString();
+
+        StockItem createdItem = objectMapper.readValue(apiResponse, StockItem.class);
+        String returnedJson = """
+                    [{
+                      "id": <ID>,
+                      "name": "Pellets",
+                      "type": "Futter",
+                      "amountInStock": 42.0,
+                      "pricePerKilo": 42.0
+                    }]
+                """.replace("<ID>", createdItem.id());
+
+        mockMvc.perform(get("/lager/ueberblick"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(returnedJson));
     }
 }
--- backend/src/test/java/de/ffmjava/capstone/backend/stock/StockServiceTest.java
+++ backend/src/test/java/de/ffmjava/capstone/backend/stock/StockServiceTest.java
@@ -2,11 +2,12 @@ package de.ffmjava.capstone.backend.stock;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class StockServiceTest {
 
@@ -20,6 +21,19 @@ class StockServiceTest {
 
         List<StockItem> expected = List.of();
         List<StockItem> actual = service.getAllStockItems();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addNewStockItem_AndExpectStockItem() {
+        StockItem newStockItem = new StockItem(null, "name", StockType.FUTTER, BigDecimal.ONE, BigDecimal.ONE);
+
+        doReturn(newStockItem.withId("1")).when(mockRepository).save(any());
+
+        StockItem actual = service.addNewStockItem(newStockItem);
+
+        StockItem expected = newStockItem.withId("1");
 
         assertEquals(expected, actual);
     }
