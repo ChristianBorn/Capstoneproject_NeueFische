d48d3c9ffbe162d181ec9a859758e1f1407f3c41
christianborn <chrisborn@hotmail.de>
2022-11-23 19:18:07 +0100

    Rebase onto master

--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
@@ -1,7 +1,9 @@
 package de.ffmjava.capstone.backend.stock;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,6 +18,16 @@ public class StockService {
         return repository.findAll();
     }
 
+    public void deleteStockItem(String id) throws ResponseStatusException{
+        if(!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Kein Eintrag für die gegebene ID gefunden");
+        }
+        try {
+            repository.deleteById(id);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Keine ID im Request vorhanden");
+        }
+    }
     public StockItem addNewStockItem(StockItem newStockItem) {
         StockItem newStockItemWithId = newStockItem.withId(UUID.randomUUID().toString());
         return repository.save(newStockItemWithId);
