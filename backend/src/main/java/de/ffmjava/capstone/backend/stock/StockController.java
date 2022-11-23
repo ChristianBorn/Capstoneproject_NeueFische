563f4c8685479b9ed0e5bd514b0e3167f807b867
christianborn <chrisborn@hotmail.de>
2022-11-23 14:37:23 +0100

    Frontend: Add validation and error messages

--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockController.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockController.java
@@ -2,8 +2,12 @@ package de.ffmjava.capstone.backend.stock;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,7 +24,20 @@ class StockController {
 
     @PostMapping("/ueberblick")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public StockItem addNewStockItem(@RequestBody StockItem newStockItem) {
-        return service.addNewStockItem(newStockItem);
-    }
-}
+    public ResponseEntity<String> addNewStockItem(@Valid @RequestBody StockItem newStockItem, Errors errors) {
+        if(errors.hasErrors()) {
+            FieldError fieldError;
+            String errorMessage = null;
+            if (errors.getFieldError() != null) {
+                fieldError = errors.getFieldError();
+                if (fieldError != null) {
+                    errorMessage = fieldError.getDefaultMessage();
+                }
+            }
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        service.addNewStockItem(newStockItem);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+
+} }
--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockItem.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockItem.java
@@ -2,13 +2,22 @@ package de.ffmjava.capstone.backend.stock;
 
 import lombok.With;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 @With
 public record StockItem(
         String id,
+        @NotBlank(message = "Feld \"Name/Bezeichnung\" darf nicht leer sein")
         String name,
+        @NotNull (message = "Feld \"Typ\" darf nicht leer sein")
         StockType type,
+        @Min(0)
+        @NotNull (message = "Feld \"Menge\" in Kilogramm darf nicht leer sein")
         BigDecimal amountInStock,
+        @Min(0)
+        @NotNull (message = "Feld \"Preis\" pro Kilogramm darf nicht leer sein")
         BigDecimal pricePerKilo
 ) {
 }
