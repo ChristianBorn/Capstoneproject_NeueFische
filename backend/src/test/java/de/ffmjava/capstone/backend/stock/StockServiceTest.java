2bbdac74911dc7603baef670f647aea90c24d1d6
christianborn <chrisborn@hotmail.de>
2022-11-23 19:46:59 +0100

    Backend: Add Unittest for unsuccessful delete

--- backend/src/test/java/de/ffmjava/capstone/backend/stock/StockServiceTest.java
+++ backend/src/test/java/de/ffmjava/capstone/backend/stock/StockServiceTest.java
@@ -1,6 +1,8 @@
 package de.ffmjava.capstone.backend.stock;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -49,6 +51,19 @@ class StockServiceTest {
         } catch (Exception e) {
             fail();
         }
-
+    }
+    @Test
+    void deleteStockItem_AndExpectException() {
+        String idToDelete = "1";
+        ResponseStatusException expectedException = new ResponseStatusException(HttpStatus.NOT_FOUND, "Kein Eintrag für die gegebene ID gefunden");
+        when(mockRepository.existsById(idToDelete))
+                .thenThrow(expectedException);
+        doNothing().when(mockRepository).deleteById(idToDelete);
+        try {
+            service.deleteStockItem(idToDelete);
+            fail();
+        } catch (ResponseStatusException e) {
+            assertEquals(expectedException, e);
+        }
     }
 }
