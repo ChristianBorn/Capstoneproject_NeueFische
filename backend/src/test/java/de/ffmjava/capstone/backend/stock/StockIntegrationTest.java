379d56f066b942a008ea846c1ba75da4bb418ffa
christianborn <chrisborn@hotmail.de>
2022-11-24 17:32:04 +0100

    Backend: add new integrationtest for successful deletion

--- backend/src/test/java/de/ffmjava/capstone/backend/stock/StockIntegrationTest.java
+++ backend/src/test/java/de/ffmjava/capstone/backend/stock/StockIntegrationTest.java
@@ -1,5 +1,6 @@
 package de.ffmjava.capstone.backend.stock;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,9 +10,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -21,6 +20,8 @@ class StockIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void getAllStockItems_AndExpectEmptyList_200() throws Exception {
@@ -28,6 +29,33 @@ class StockIntegrationTest {
                         ("/stock/overview"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void deleteStockItem_AndExpect_204() throws Exception {
+        String jsonString =
+                """
+                            {
+                              "name": "Pellets",
+                              "type": "Futter",
+                              "amountInStock": 42.0,
+                              "pricePerKilo": 42.0
+                            }
+                        """;
+        mockMvc.perform(post("/stock/overview")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString)
+        );
+
+        String getResponse = mockMvc.perform(get
+                        ("/stock/overview")).andReturn().getResponse().getContentAsString().replace("[", "")
+                .replace("]", "");
+        String idToDelete = objectMapper.readValue(getResponse, StockItem.class).id();
+
+        mockMvc.perform(delete
+                        ("/stock/overview/" + idToDelete))
+                .andExpect(status().isNoContent());
     }
 
     @Test
@@ -82,8 +110,5 @@ class StockIntegrationTest {
                 )
                 .andExpect(status().is(400))
                 .andExpect(content().string("Feld \"Name/Bezeichnung\" darf nicht leer sein"));
-
-
     }
-
 }
