c00088a2513c53a7e4a062da773a3186be084acf
christianborn <chrisborn@hotmail.de>
2022-11-25 17:30:28 +0100

    Backend: Fix code smell

--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
@@ -3,7 +3,6 @@ package de.ffmjava.capstone.backend.stock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
