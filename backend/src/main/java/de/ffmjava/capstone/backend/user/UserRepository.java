8b8df941ae8c998a0d65a1e928a4db4790fc08a5
christianborn <chrisborn@hotmail.de>
2022-11-28 14:24:26 +0100

    Backend: Endpoints for login and registering

---
+++ backend/src/main/java/de/ffmjava/capstone/backend/user/UserRepository.java
@@ -0,0 +1,7 @@
+package de.ffmjava.capstone.backend.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<AppUser, String> {
+    AppUser findByUsername(String username);
+}
---
+++ backend/src/main/java/de/ffmjava/capstone/backend/user/UserService.java
@@ -0,0 +1,54 @@
+package de.ffmjava.capstone.backend.user;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public AppUser findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public String save(AppUser newAppUser, PasswordEncoder passwordEncoder) throws UserAlreadyExistsException {
+        if (userRepository.findByUsername(newAppUser.username()) != null) {
+            throw new UserAlreadyExistsException("""
+                    {"userAlreadyExists": "User with that username already exists"}
+                    """);
+        }
+        AppUser appUser = newAppUser
+                .withId(UUID.randomUUID().toString())
+                .withRole("Basic")
+                .withPasswordBcrypt(passwordEncoder.encode(newAppUser.rawPassword()))
+                .withRawPassword("");
+        userRepository.save(appUser);
+        return "Created user: " + newAppUser.username();
+    }
+
+    public String getUserDetails(String username) {
+        if (!username.equals("anonymousUser")) {
+            String eMail;
+            try {
+                eMail = userRepository.findByUsername(username).eMail();
+            } catch (NullPointerException e) {
+                eMail = "";
+            }
+
+            return """
+                    {"username":"<username>",
+                    "eMail":"<eMail>"}
+                    """.replace("<username>", username).replace("<eMail>", eMail);
+        }
+        return """
+                {"username":"<username>"}
+                """.replace("<username>", username);
+    }
+}
