3a01daca950bfb89e15a4a45ea693f4a49970cce
christianborn <chrisborn@hotmail.de>
2022-11-28 14:21:57 +0100

    Backend: Setup for Security

--- backend/src/main/java/de/ffmjava/capstone/backend/SecurityConfig.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/SecurityConfig.java
@@ -1,16 +1,27 @@
 package de.ffmjava.capstone.backend;
 
+import de.ffmjava.capstone.backend.user.AppUser;
+import de.ffmjava.capstone.backend.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@RequiredArgsConstructor
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private static final String PROTECTED_STOCK_PATH = "/stock**";
+    private static final String ROLE_BASIC = "Basic";
+
+    private final UserService userService;
 
     public static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -19,15 +30,76 @@ public class SecurityConfig {
         return passwordEncoder;
     }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
                 .httpBasic().and()
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers(HttpMethod.POST, "/api/app-users").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/app-users/me").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        PROTECTED_STOCK_PATH
+                ).hasAnyRole(ROLE_BASIC)
+                .antMatchers(HttpMethod.POST,
+                        PROTECTED_STOCK_PATH).hasAnyRole(ROLE_BASIC)
+                .antMatchers(HttpMethod.PUT,
+                        PROTECTED_STOCK_PATH).hasAnyRole(ROLE_BASIC)
+                .antMatchers(HttpMethod.DELETE,
+                        PROTECTED_STOCK_PATH).hasAnyRole(ROLE_BASIC)
+                .anyRequest().denyAll()
+                .and().formLogin()
                 .and().build();
     }
 
 
+    @Bean
+    public UserDetailsManager userDetailsService() {
+        return new UserDetailsManager() {
+            static final String EXCEPTIONMESSAGE = "Method invocation not possible";
+
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser userByName = userService.findByUsername(username);
+                if (userByName == null) {
+                    throw new UsernameNotFoundException("Username not found");
+                }
+                return User.builder()
+                        .username(username)
+                        .password(userByName.passwordBcrypt())
+                        .roles(userByName.role())
+                        .build();
+            }
+
+            @Override
+            public void createUser(UserDetails user) {
+                throw new UnsupportedOperationException(EXCEPTIONMESSAGE);
+            }
+
+            @Override
+            public void updateUser(UserDetails user) {
+                throw new UnsupportedOperationException(EXCEPTIONMESSAGE);
+            }
+
+            @Override
+            public void deleteUser(String username) {
+                throw new UnsupportedOperationException(EXCEPTIONMESSAGE);
+            }
+
+            @Override
+            public void changePassword(String oldPassword, String newPassword) {
+                throw new UnsupportedOperationException(EXCEPTIONMESSAGE);
+            }
+
+            @Override
+            public boolean userExists(String username) {
+                throw new UnsupportedOperationException(EXCEPTIONMESSAGE);
+            }
+        };
+
+    }
+
+
 }
+
