b60f559ead1eadf6093b30298aaba6a2c247df7e
christianborn <chrisborn@hotmail.de>
2022-11-28 14:24:26 +0100

    Backend: Endpoints for login and registering

---
+++ backend/src/main/java/de/ffmjava/capstone/backend/user/UserController.java
@@ -0,0 +1,63 @@
+package de.ffmjava.capstone.backend.user;
+
+import de.ffmjava.capstone.backend.SecurityConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/app-users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    @GetMapping("/login")
+    public ResponseEntity<String> login() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpSession httpSession) {
+        httpSession.invalidate();
+    }
+
+    @GetMapping("/me")
+    public String me() {
+        String username = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        return service.getUserDetails(username);
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<String> registration(@Valid @RequestBody AppUser newAppUser, Errors errors) {
+        if (errors.hasErrors()) {
+            FieldError fieldError;
+            String errorMessage = null;
+            if (errors.getFieldError() != null) {
+                fieldError = errors.getFieldError();
+                if (fieldError != null) {
+                    errorMessage = fieldError.getDefaultMessage();
+                }
+            }
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        try {
+            service.save(newAppUser, SecurityConfig.passwordEncoder);
+            return new ResponseEntity<>("User was registered succesfully!", HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+}
+
