278c0533ff97324adc5e30965d2326fc7e4c63df
christianborn <chrisborn@hotmail.de>
2022-11-28 17:18:58 +0100

    Backend: Add validation for user fields

--- backend/src/main/java/de/ffmjava/capstone/backend/user/AppUser.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/user/AppUser.java
@@ -3,10 +3,14 @@ package de.ffmjava.capstone.backend.user;
 import com.mongodb.lang.Nullable;
 import lombok.With;
 
+import javax.validation.constraints.NotBlank;
+
 @With
 public record AppUser(
         String id,
+        @NotBlank(message = "Username darf nicht leer sein")
         String username,
+        @NotBlank(message = "Passwort darf nicht leer sein")
         String rawPassword,
         String passwordBcrypt,
         String role,
