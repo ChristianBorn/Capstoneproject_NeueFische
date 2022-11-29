9190481a0c2510209571bb89796a99984a52b8fe
christianborn <chrisborn@hotmail.de>
2022-11-29 14:07:34 +0100

    Frontend: Add registerpage with specific error messages

--- frontend/src/security/RegisterPage.tsx
+++ frontend/src/security/RegisterPage.tsx
@@ -1,38 +1,63 @@
 import React, {useState} from 'react';
 import axios from "axios";
+import FieldLabelGroup from "../structuralComponents/FieldLabelGroup";
+import {Link} from "react-router-dom";
+import "./css/RegisterPage.css"
 
 
 function RegisterPage() {
     const [username, setUsername] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
+    const [rawPassword, setRawPassword] = useState<string>("")
     const [registrationErrorMessage, setRegistrationErrorMessage] = useState({
-        password: undefined,
+        rawPassword: undefined,
         username: undefined,
         userAlreadyExists: undefined
     })
     const [successMessage, setSuccessMessage] = useState<string>("")
 
     const register = () => {
-        axios.post("/api/app-users", {username, password})
+        axios.post("/api/app-users", {username, rawPassword})
             .then(response => response.data)
             .then(data => setSuccessMessage(data))
+            .then(() => setRegistrationErrorMessage({
+                rawPassword: undefined,
+                username: undefined,
+                userAlreadyExists: undefined,
+            }))
             .catch(response => {
                 setRegistrationErrorMessage(response.response.data)
+                setSuccessMessage("")
             })
+
     }
 
 
     return <>
-        <label htmlFor={"username"}>Username</label>
-        <input required id={"username"} type={"text"} onChange={event => setUsername(event.target.value)}/>
-        {registrationErrorMessage.username && <p>{registrationErrorMessage.username}</p>}
-        <label htmlFor={"password"}>Password</label>
-        <input required id={"password"} type={"password"} onChange={event => setPassword(event.target.value)}/>
-        {registrationErrorMessage.password && <p>{registrationErrorMessage.password}</p>}
-        <button onClick={() => register()}>Register</button>
-        {successMessage && <p>{successMessage}</p>}
-        {registrationErrorMessage.userAlreadyExists && <p>{registrationErrorMessage.userAlreadyExists}</p>}
+        <div className={"login-form-area"}>
+            <h2>Registrierung</h2>
+            <FieldLabelGroup>
+                <label htmlFor={"username"}>Username</label>
+                <input required id={"username"} type={"text"} onChange={event => setUsername(event.target.value)}/>
+                {registrationErrorMessage.username &&
+                    <p className={"error-message"}>{registrationErrorMessage.username}</p>}
 
+            </FieldLabelGroup>
+            <FieldLabelGroup>
+                <label htmlFor={"password"}>Passwort</label>
+                <input required id={"password"} type={"password"}
+                       onChange={event => setRawPassword(event.target.value)}/>
+                {registrationErrorMessage.rawPassword &&
+                    <p className={"error-message"}>{registrationErrorMessage.rawPassword}</p>}
+
+            </FieldLabelGroup>
+            {registrationErrorMessage.userAlreadyExists &&
+                <p className={"error-message"}>{registrationErrorMessage.userAlreadyExists}</p>}
+            <button className={"submit-button"} onClick={() => register()}>Registrieren</button>
+            {successMessage && <p>{successMessage}</p>}
+
+
+            <Link to="/">Zurück zu login</Link>
+        </div>
     </>
 }
 
