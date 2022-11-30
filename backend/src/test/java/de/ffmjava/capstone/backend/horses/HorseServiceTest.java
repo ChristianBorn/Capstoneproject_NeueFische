12e115e5edec388414d1da2f974fd9db5294dd25
christianborn <chrisborn@hotmail.de>
2022-11-30 13:05:58 +0100

    Backend: Add tests

---
+++ backend/src/test/java/de/ffmjava/capstone/backend/horses/HorseServiceTest.java
@@ -0,0 +1,37 @@
+package de.ffmjava.capstone.backend.horses;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HorseServiceTest {
+
+    private final HorseRepository mockRepository = mock(HorseRepository.class);
+    private final HorseService service = new HorseService(mockRepository);
+
+    @Test
+    void getAllHorses() {
+        when(mockRepository.findAll()).thenReturn(List.of());
+
+        List<Horse> expected = List.of();
+        List<Horse> actual = service.getAllHorses();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testHorseRecord() {
+        Horse newHorse = new Horse("id", "name", "owner", Map.of("key", new BigDecimal("0.0")));
+        Horse changedHorse = newHorse.withId("1").withName("Peter").withOwner("Hansi").withConsumption(Map.of("Hafer", new BigDecimal("4.0")));
+        Horse expected = changedHorse;
+        Horse actual = changedHorse;
+
+        assertEquals(expected, actual);
+    }
+}
