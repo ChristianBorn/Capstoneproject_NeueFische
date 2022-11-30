967aff0b402338ca56b1373d9496cd71092e878b
christianborn <chrisborn@hotmail.de>
2022-11-30 22:48:24 +0100

    Backend: Fix error in logs because of missing ids for consumption objects

--- backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Consumption.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Consumption.java
@@ -1,11 +1,11 @@
 package de.ffmjava.capstone.backend.horses.model;
 
-import org.springframework.data.annotation.Id;
+import lombok.With;
 
 import java.math.BigDecimal;
 
+@With
 public record Consumption(
-        @Id
         String id,
         String name,
         BigDecimal dailyConsumption
