588886cbea094c06e7ac7e95eb288d96a835c6a4
christianborn <chrisborn@hotmail.de>
2022-11-30 15:50:09 +0100

    Backend: Fix tests

--- backend/src/test/java/de/ffmjava/capstone/backend/horses/HorseIntegrationTest.java
+++ backend/src/test/java/de/ffmjava/capstone/backend/horses/HorseIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +21,7 @@ class HorseIntegrationTest {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(roles = "Basic")
     void getAllHorses() throws Exception {
         mockMvc.perform(get
                         ("/horses/overview"))
