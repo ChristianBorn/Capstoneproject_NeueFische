951204e0de1949420c526dba15ca7eef0a759d26
christianborn <chrisborn@hotmail.de>
2022-12-01 15:37:51 +0100

    Backend: add static method for error check, add PUT Endpoint for horses, add validation to Horse

--- backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Horse.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Horse.java
@@ -3,13 +3,16 @@ package de.ffmjava.capstone.backend.horses.model;
 import lombok.With;
 import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @With
 public record Horse(
         @Id
         String id,
+        @NotBlank(message = "Feld \"Name\" darf nicht leer sein")
         String name,
+        @NotBlank(message = "Feld \"Besitzer\" darf nicht leer sein")
         String owner,
         List<Consumption> consumption
 ) {
