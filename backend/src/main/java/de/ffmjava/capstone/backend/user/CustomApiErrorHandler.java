3949c99d988cf436d7b3497078abc7d34ba23b4b
christianborn <chrisborn@hotmail.de>
2022-12-01 13:32:06 +0100

    Backend: Fix bug in CustomApiErrorHandler to make it return multiple error messages

--- backend/src/main/java/de/ffmjava/capstone/backend/user/CustomApiErrorHandler.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/user/CustomApiErrorHandler.java
@@ -3,29 +3,29 @@ package de.ffmjava.capstone.backend.user;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.lang.Nullable;
 import lombok.Data;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class CustomApiErrorHandler {
-    @Nullable
-    private ArrayList<String> fields = new ArrayList<>();
+
     private Map<String, ArrayList<String>> fieldMessageMappings = new HashMap<>();
     private String jsonString;
     private Errors errors;
 
     public CustomApiErrorHandler(Errors errors) {
         for (FieldError singleError : errors.getFieldErrors()) {
-            if (!fields.contains(singleError.getField())) {
+            if (!fieldMessageMappings.containsKey(singleError.getField())) {
                 fieldMessageMappings.put(singleError.getField(), new ArrayList<>());
             }
             fieldMessageMappings.get(singleError.getField()).add(singleError.getDefaultMessage());
+            Collections.sort(fieldMessageMappings.get(singleError.getField()));
         }
         try {
             this.jsonString = new ObjectMapper().writeValueAsString(fieldMessageMappings);
