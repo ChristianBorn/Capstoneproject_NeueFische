8ab1fe025675941be89391e33072f2ac9ca7f476
christianborn <chrisborn@hotmail.de>
2022-12-01 16:24:57 +0100

    Frontend: Add add button and modal

--- frontend/src/horses/HorseOverview.tsx
+++ frontend/src/horses/HorseOverview.tsx
@@ -2,10 +2,22 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {HorseModel} from "./HorseModel";
 import ClipLoader from "react-spinners/ClipLoader";
+import AddIcon from "../icons/AddIcon";
+import AddHorseModal from "./AddHorseModal";
 
 function HorseOverview() {
 
     const [horses, setHorses] = useState<HorseModel[]>()
+    const [addModalIsOpen, setAddModalIsOpen] = useState<boolean>(false)
+    const [successMessage, setSuccessMessage] = useState<string>()
+
+    const openAddModal = () => {
+        setAddModalIsOpen(true)
+        setSuccessMessage("")
+    }
+    const closeModal = () => {
+        setAddModalIsOpen(false)
+    }
 
 
     const getAllHorses = () => {
@@ -31,7 +43,11 @@ function HorseOverview() {
 
     return (
         <>
-
+            <AddHorseModal modalIsOpen={addModalIsOpen}
+                           closeModal={closeModal}
+                           reloadHorses={getAllHorses}
+                           setSuccessMessage={setSuccessMessage}
+            />
             {horses.length > 0 ?
                 <>
                     <div className={"overview-table-wrapper"}>
@@ -66,10 +82,14 @@ function HorseOverview() {
                         </table>
 
                     </div>
-                </>
+                    {successMessage && <div className={"success-message"}>{successMessage}</div>}
+                    <AddIcon openModal={openAddModal}/></>
+
                 :
                 <div>
+                    {successMessage && <div className={"success-message"}>{successMessage}</div>}
                     <p>Keine Pferde im Stall</p>
+                    <AddIcon openModal={openAddModal}/>
                 </div>
 
             }
