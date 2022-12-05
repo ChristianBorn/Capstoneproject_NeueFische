d0e1f7ddd354315e07cc68fcc874b517ff621c8e
christianborn <chrisborn@hotmail.de>
2022-12-05 11:03:21 +0100

    Frontend & Backend: Rename consumption field

--- backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Horse.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Horse.java
@@ -14,6 +14,6 @@ public record Horse(
         String name,
         @NotBlank(message = "Feld \"Besitzer\" darf nicht leer sein")
         String owner,
-        List<Consumption> consumptionList
+        List<Consumption> consumption
 ) {
 }
