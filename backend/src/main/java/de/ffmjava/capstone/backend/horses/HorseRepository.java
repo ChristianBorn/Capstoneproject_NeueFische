ea07e69dccbf8217f1650fb5732990f044d8152b
christianborn <chrisborn@hotmail.de>
2022-12-07 15:43:58 +0100

    Backend: Delete consumption item from consumption list if corresponding stockitem was deleted

--- backend/src/main/java/de/ffmjava/capstone/backend/horses/HorseRepository.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/horses/HorseRepository.java
@@ -2,6 +2,13 @@ package de.ffmjava.capstone.backend.horses;
 
 import de.ffmjava.capstone.backend.horses.model.Horse;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface HorseRepository extends MongoRepository<Horse, String> {
+    @Query("{ 'consumption.id':  ?0}")
+    List<Horse> findHorsesByConsumptionId(String id);
 }
