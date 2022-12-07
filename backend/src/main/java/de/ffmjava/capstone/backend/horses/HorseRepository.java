4493016d1f69ec2d2707d7e8d244667969528267
christianborn <chrisborn@hotmail.de>
2022-12-07 19:46:58 +0100

    Backend: add Aggregation for consumption of StockItems

--- backend/src/main/java/de/ffmjava/capstone/backend/horses/HorseRepository.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/horses/HorseRepository.java
@@ -1,14 +1,23 @@
 package de.ffmjava.capstone.backend.horses;
 
 import de.ffmjava.capstone.backend.horses.model.Horse;
+import org.bson.Document;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import org.springframework.data.mongodb.repository.Query;
+
+
 
 @Repository
 public interface HorseRepository extends MongoRepository<Horse, String> {
+    @Aggregation(pipeline = {
+            "{'$unwind': {'path': '$consumptionList'}}",
+            "{'$group':  {'_id': '$consumptionList.name', 'id': {'name': '$consumptionList.name', 'Verbrauch': {'$sum': '$consumptionList.dailyConsumption'}}}}"
+    })
+    List<Document> aggregateConsumptions();
     @Query("{ 'consumption.id':  ?0 }")
     List<Horse> findHorsesByConsumptionId(String id);
 }
