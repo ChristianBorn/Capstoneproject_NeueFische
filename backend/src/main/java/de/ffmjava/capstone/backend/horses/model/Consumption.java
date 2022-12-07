ba9219e26dc3323d11135da20e99f69b5b0a9373
christianborn <chrisborn@hotmail.de>
2022-12-07 09:46:36 +0100

    Backend: Change field dailyConsumption so mongoDB saves it as decimal

--- backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Consumption.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/horses/model/Consumption.java
@@ -1,13 +1,17 @@
 package de.ffmjava.capstone.backend.horses.model;
 
 import lombok.With;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.math.BigDecimal;
+
+import static org.springframework.data.mongodb.core.mapping.FieldType.DECIMAL128;
 
 @With
 public record Consumption(
         String id,
         String name,
+        @Field(targetType = DECIMAL128)
         BigDecimal dailyConsumption
 ) {
 }
