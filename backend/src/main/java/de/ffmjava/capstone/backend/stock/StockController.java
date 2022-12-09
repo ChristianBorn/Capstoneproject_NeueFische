b72a36cf5bc17a3625fb351c985ae878929c1620
christianborn <chrisborn@hotmail.de>
2022-12-09 16:02:52 +0100

    Backend: Modify endpoint to return a map instead of list

--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockController.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/stock/")
--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
