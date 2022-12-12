a1fa23a87cb9b2e485e712548da2e7a362e288c7
christianborn <chrisborn@hotmail.de>
2022-12-12 13:09:15 +0100

    Backend: Add Caching

--- backend/src/main/java/de/ffmjava/capstone/backend/BackendApplication.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/BackendApplication.java
@@ -3,10 +3,12 @@ package de.ffmjava.capstone.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class BackendApplication {
 
     public static void main(String[] args) {
