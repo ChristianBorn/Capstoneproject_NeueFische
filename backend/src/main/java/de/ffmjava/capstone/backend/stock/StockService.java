ba4ee639afd3d08ea8414ee0c86d3b926a4ef055
christianborn <chrisborn@hotmail.de>
2022-12-12 12:48:48 +0100

    Backend: Add Cronjob that subtracts the consumption from the amounts in stock

--- backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/stock/StockService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
