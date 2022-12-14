22a12b0b40bb66c6f3478fee6d924a22a62bd531
christianborn <chrisborn@hotmail.de>
2022-12-14 17:32:17 +0100

    Backend: Add PUT endpoint and tests

--- backend/src/main/java/de/ffmjava/capstone/backend/clients/ClientRepository.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/clients/ClientRepository.java
@@ -1,9 +1,11 @@
 package de.ffmjava.capstone.backend.clients;
 
 import de.ffmjava.capstone.backend.clients.model.Client;
+import de.ffmjava.capstone.backend.horses.model.Horse;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends MongoRepository<Client, String> {
+    boolean existsByOwnsHorseContains(Horse horseOfUpdatedClient);
 }
