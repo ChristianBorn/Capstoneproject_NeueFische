4ad1410d6b5a0d1816c6a96f7a461cf2bbcfb88b
christianborn <chrisborn@hotmail.de>
2022-12-18 12:34:55 +0100

    Fix Code Smell

--- frontend/src/security/RegisterPage.tsx
+++ frontend/src/security/RegisterPage.tsx
@@ -49,7 +49,7 @@ function RegisterPage() {
                 {registrationErrorMessage.rawPassword &&
                     <div className={"message-container"}>
                         {registrationErrorMessage.rawPassword.map(errorMessage =>
-                            <p key={Math.random() * 100000} className={"error-message"}>{errorMessage}</p>
+                            <p key={errorMessage} className={"error-message"}>{errorMessage}</p>
                         )}
                     </div>}
 
