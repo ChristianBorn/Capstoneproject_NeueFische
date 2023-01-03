191fe8fe0569f1f2a74d0f63c3bf86e9020a8c4f
ChristianBorn <chrisborn@hotmail.de>
2023-01-03 16:27:28 +0100

    Backend: Introduce ClientDTO

--- backend/src/main/java/de/ffmjava/capstone/backend/clients/ClientRepository.java
+++ backend/src/main/java/de/ffmjava/capstone/backend/clients/ClientRepository.java
@@ -1,11 +1,10 @@
 package de.ffmjava.capstone.backend.clients;
 
 import de.ffmjava.capstone.backend.clients.model.Client;
-import de.ffmjava.capstone.backend.horses.model.Horse;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends MongoRepository<Client, String> {
-    Client findByOwnsHorseContains(Horse horseOfUpdatedClient);
+    Client findByOwnsHorseContains(String horseOfUpdatedClientId);
 }
