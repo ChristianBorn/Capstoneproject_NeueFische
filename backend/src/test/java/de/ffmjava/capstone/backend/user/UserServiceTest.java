154808d122aeb2374dd17eed8c7e25708d849a06
ChristianBorn <chrisborn@hotmail.de>
2023-04-18 12:54:28 +0200

    Backend: Add tests

--- backend/src/test/java/de/ffmjava/capstone/backend/user/UserServiceTest.java
+++ backend/src/test/java/de/ffmjava/capstone/backend/user/UserServiceTest.java
@@ -1,6 +1,7 @@
 package de.ffmjava.capstone.backend.user;
 
 import de.ffmjava.capstone.backend.user.model.AppUser;
+import de.ffmjava.capstone.backend.user.model.AppUserDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -10,17 +11,19 @@ import static org.mockito.Mockito.*;
 class UserServiceTest {
 
     private final UserRepository mockUserRepository = mock(UserRepository.class);
-
     private final BCryptPasswordEncoder mockPasswordEncoder = mock(BCryptPasswordEncoder.class);
     private final UserService userService = new UserService(mockUserRepository);
 
 
     @Test
     void saveSuccessful() {
+        //Given
         AppUser newAppUser = new AppUser("null", "username", "password", "", "", "email");
         newAppUser = newAppUser.withEMail("email@email.de").withUsername("Different username");
+        //When
         when(mockUserRepository.save(newAppUser)).thenReturn(newAppUser);
         when(mockPasswordEncoder.encode("password")).thenReturn("encodedPassword");
+        //Then
         String actual = userService.save(newAppUser, mockPasswordEncoder);
         String expected = "Created user: " + newAppUser.username();
 
@@ -28,14 +31,29 @@ class UserServiceTest {
         verify(mockPasswordEncoder).encode("password");
     }
 
-
     @Test
     void findByUsername() {
+        //Given
         AppUser appUser = new AppUser("null", "username", "password", "", "", "email");
+        //When
         when(mockUserRepository.findByUsername("username")).thenReturn(appUser);
-
+        //Then
         AppUser expected = appUser;
         AppUser actual = userService.findByUsername("username");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getDTOByUsername() {
+        //Given
+        AppUser appUser = new AppUser("null", "username", "password", "", "", "email");
+        AppUserDTO appUserDTO = new AppUserDTO("null", "username", "", "email");
+        //When
+        when(mockUserRepository.findByUsername("username")).thenReturn(appUser);
+        //Then
+        AppUserDTO expected = appUserDTO;
+        AppUserDTO actual = userService.getDTOByUsername("username");
 
         assertEquals(expected, actual);
     }
